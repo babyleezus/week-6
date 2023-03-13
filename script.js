5742fd1320d75f8edce1533b786949c1f4dfbe30
Baby Leezus <118462761+babyleezus@users.noreply.github.com>
2023-03-13 14:43:00 +0000

    added input elements to html and pseudocode

---
+++ script.js
@@ -0,0 +1,82 @@
+/* store questions in an array of objects 
+add event listener to start button 
+link high score sheet to high scores buttom 
+create timer
+automatically load next question when answer is selected 
+*/
+
+let start = document.getElementById('start')
+start.addEventListener('click' function() {
+    //if start is clicked, current elements should get hidden and move on to question screen
+
+}) 
+
+const questionsData [
+    {   question: What is the most used programming language in the world?,
+        a: Python,
+        b: C++,
+        c: JavaScript,
+        d: 
+    }
+    {   question 2: Which of the following is not a primitive data type in javascript?
+        a: strings.
+        b: booleans,
+        c: arrays,
+        d: numbers
+    }
+    {   question 3: What would the output of the following code be?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    {   question 4: ?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    {   question 5: ?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    {   question 5: ?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    {   question 6: ?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    {   question 7: ?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    {   question 8: ?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    {   question 9: ?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    {   question 10: ?,
+        a: .
+        b: .
+        c: .
+        d: 
+    }
+    
+]
