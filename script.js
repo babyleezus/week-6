0dd175e924136d8d7d0e24921b9623939cd427a6
Baby Leezus <118462761+babyleezus@users.noreply.github.com>
2023-03-14 23:04:25 +0000

    edited questions array and added function to display them

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 const start = document.getElementById('start');
 const questions = document.getElementById('questions');
 
+
 const startTimer = 2;
 let timer = startTimer * 60;
 
 const countdownEl = document.getElementById('time');
 
 
-
+//function to show timer 
 function countdownTimer() {
     const minutes = Math.floor(timer/60);
     let seconds = timer % 60;
@@ -17,68 +18,111 @@ function countdownTimer() {
     timer--; 
 }
 
+start.addEventListener('click', startQuiz); //quiz begins when you press the buttion 
 
 
-start.addEventListener('click', startQuiz);
-
+//function to change the start screen adn load questions when you click the start button
 function startQuiz() {
-    console.log('started')
     start.classList.add('hide')
     questions.classList.remove('hide')
     setInterval(countdownTimer, 1000);
+    displayQuestions(1);
 }
+
+
+
+
+function displayQuestions (i) {
+    const question = document.getElementById('question-title');
+    const options = document.getElementById('choices');
+    let quizQuestion = quizQuestions[i].question;
+    let choices =   quizQuestions[i].answers; 
+                    
+    question.innerHTML = quizQuestion;
+    options.innerHTML = choices
+
+}
+
+
 
 const quizQuestions = [
     {
         question: 'What is the programming language used in the browser?',
-        a: 'Python',
-        b: 'JavaScript',
-        c: 'C++',
-        d: 'Rust',
-        answer: b
+        answer: 'JavaScript',
+        answers: [
+        'Python',
+        'JavaScript',
+        'C++',
+        'Rust'
+        ]
     },
     {
         question: 'Which of these is not a primitive data type?',
-        a: 'objects',
-        b: 'numbers',
-        c: 'strings',
-        d: 'booleans',
-        answer: a
-    },
-    {
-        question: 'Which of the following is NOT the correct format for a function',
-        a: 'Python',
-        b: 'JavaScript',
-        c: 'C++',
-        d: 'Rust'
+        answer: 'objects',
+        answers: [
+        'objects',
+        'numbers',
+        'strings',
+        'booleans'
+        ]
         
     },
     {
+        question: 'Which of the following is NOT the correct format for a function?',
+        answer: '',
+        answers: [
+        'Python',
+        'JavaScript',
+        'C++',
+        'Rust'
+        ]
+    },
+    {
         question: 'What does API stand for?',
-        a: 'Advanced Parameter Intialisation',
-        b: 'Assigned Primitive Integer',
-        c: 'Applied Prototype Integration',
-        d: 'Application Programming Interface'
+        answer: 'Application Programming Interface',
+        answers: [
+        'Advanced Parameter Intialisation',
+        'Assigned Primitive Integer',
+        'Applied Prototype Integration',
+        'Application Programming Interface'
+        ]
     },
     {
         question: 'Which of the following is NOT a type of loop in JavaScript?',
-        a: 'Do...while...',
-        b: 'For... when...',
-        c: 'While...',
-        d: 'For... in ...'
+        answer: 'For... when...',
+        answers: [
+        'Do...while...',
+        'For... when...',
+        'While...',
+        'For... in ...'
+        ]
     },
     {
         question: 'What method is used to ',
-        a: 'Python',
-        b: 'JavaScript',
-        c: 'C++',
-        d: 'Rust'
+        answer: '',
+        answers: [
+        'Python',
+        'JavaScript',
+        'C++',
+        'Rust'
+        ]
     },
     {
         question: 'What is the programming language used in the browser?',
-        a: 'Python',
-        b: 'JavaScript',
-        c: 'C++',
-        d: 'Rust'
+        answer: '',
+        answers: [
+        'Python',
+        'JavaScript',
+        'C++',
+        'Rust'
+        ]
     }
 ]
+
+
+
+//function that when you click on the answer the next button appears 
+
+//function for when the question are finished/timer is up then the quiz/timer stops
+
+//function for end screen when all questions are complete 
